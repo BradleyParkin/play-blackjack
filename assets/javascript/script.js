5bc315d2f36125fd4185686a5d1a67e4e330d23e
BradleyParkin <bradbusiness@hotmail.com>
2022-12-23 20:15:41 +0000

    update the console.log on less than 21

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,11 @@
 let firstCard = 11
 let secondCard = 10
-sum = firstCard + secondCard
+let sum = firstCard + secondCard
 
 if (sum < 21) {
     console.log("Are you going to risk a new card?")
 } else if (sum === 21) {
     console.log("OMG! You got blackjack! How lucky! Play again?")
-} if (sum > 22) {
+} else if (sum > 21) {
     console.log("Ooooo! Unlucky this time! You're bust!")
 }
