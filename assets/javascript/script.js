b46da119511c8754eed1f280b79f11d01a6662c9
BradleyParkin <bradbusiness@hotmail.com>
2023-01-01 14:06:52 +0000

    add the boolean blackjackwinner and playernoblackjack to the if statements

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,16 @@
 let firstCard = 11
 let secondCard = 10
 let sum = firstCard + secondCard
+let blackJackWinner = false
+let playerNoBlackJack = true
+
 
 if (sum < 21) {
     console.log("Are you going to risk a new card?")
 } else if (sum === 21) {
     console.log("OMG! You got blackjack! How lucky! Play again?")
+    blackJackWinner = true
 } else if (sum > 21) {
     console.log("Ooooo! Unlucky this time! You're bust!")
+    playerNoBlackJack = false
 }
