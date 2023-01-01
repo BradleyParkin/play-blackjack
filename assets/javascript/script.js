1d2e0684d93212c463ea17f698b4de98c2f5490a
BradleyParkin <bradbusiness@hotmail.com>
2023-01-01 14:45:02 +0000

    add the score element to show the scores of both cards when start game button is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,16 +5,18 @@ let blackJackWinner = false
 let playerNoBlackJack = true
 let textOutput = ""
 let messageElement = document.getElementById("message-element")
+let scoreElement = document.getElementById("score-element")
 
 
 function beginGame() {
+    scoreElement.textContent = "Your Total Score: " + sum
     if (sum < 21) {
         textOutput = "Are you going to risk a new card?"
     } else if (sum === 21) {
         textOutput = "OMG! You got blackjack! How lucky! Play again?"
         blackJackWinner = true
     } else if (sum > 21) {
-        textOutput = "Ooooo! Unlucky this time! You're bust!"
+        textOutput = "Ooooo! Unlucky this time! You're bust! Play Again?"
         playerNoBlackJack = false
     }  
     messageElement.textContent = textOutput
