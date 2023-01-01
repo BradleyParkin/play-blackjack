fabcea5cbbd8d16c7fbd4d61d9d59fd8935e2da1
BradleyParkin <bradbusiness@hotmail.com>
2023-01-01 14:27:06 +0000

    add the begin game function with the start button to start the game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,14 +3,19 @@ let secondCard = 10
 let sum = firstCard + secondCard
 let blackJackWinner = false
 let playerNoBlackJack = true
+let textOutput = ""
 
 
-if (sum < 21) {
-    console.log("Are you going to risk a new card?")
-} else if (sum === 21) {
-    console.log("OMG! You got blackjack! How lucky! Play again?")
-    blackJackWinner = true
-} else if (sum > 21) {
-    console.log("Ooooo! Unlucky this time! You're bust!")
-    playerNoBlackJack = false
+function beginGame() {
+    if (sum < 21) {
+        textOutput = "Are you going to risk a new card?"
+    } else if (sum === 21) {
+        textOutput = "OMG! You got blackjack! How lucky! Play again?"
+        blackJackWinner = true
+    } else if (sum > 21) {
+        textOutput = "Ooooo! Unlucky this time! You're bust!"
+        playerNoBlackJack = false
+    }  
+    console.log(textOutput)
 }
+
